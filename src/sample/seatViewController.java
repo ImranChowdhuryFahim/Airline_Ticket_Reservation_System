8f33c0b15ec2de9ad96bb3ddd9cdf1d0f51b16e6
tahsin <tahsinbd.ahmed1@gmail.com>
2019-10-28 21:54:05 +0600

    seatview controller done (button code e nai)(button er id o nai)

--- src/sample/seatViewController.java
+++ src/sample/seatViewController.java
@@ -1,13 +1,13 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
 import java.awt.*;
+import org.omg.CORBA.PRIVATE_MEMBER;
 
 public class seatViewController {
 
@@ -15,39 +15,570 @@ public class seatViewController {
     private ImageView F12;
 
     @FXML
+    private ImageView F13;
+
+    @FXML
+    private ImageView F14;
+
+    @FXML
     private ImageView F15;
+
+    @FXML
+    private ImageView F18;
 
     @FXML
     private ImageView F11;
 
     @FXML
-    private TextField F13;
+    private ImageView F16;
+
+    @FXML
+    private ImageView F17;
 
     @FXML
     private ImageView F21;
 
     @FXML
+    private ImageView F22;
+
+    @FXML
+    private ImageView F23;
+
+    @FXML
+    private ImageView F25;
+
+    @FXML
+    private ImageView F26;
+
+    @FXML
+    private ImageView F27;
+
+    @FXML
+    private ImageView F24;
+
+    @FXML
+    private ImageView F28;
+
+    @FXML
+    private ImageView B11;
+
+    @FXML
+    private ImageView B12;
+
+    @FXML
+    private ImageView B13;
+
+    @FXML
+    private ImageView B14;
+
+    @FXML
+    private ImageView B18;
+
+    @FXML
+    private ImageView B15;
+
+    @FXML
+    private ImageView B17;
+
+    @FXML
+    private ImageView B16;
+
+    @FXML
+    private ImageView B21;
+
+    @FXML
+    private ImageView B25;
+
+    @FXML
+    private ImageView B26;
+
+    @FXML
+    private ImageView B27;
+
+    @FXML
+    private ImageView B28;
+
+    @FXML
+    private ImageView B24;
+
+    @FXML
+    private ImageView B22;
+
+    @FXML
+    private ImageView B23;
+
+    @FXML
+    private ImageView E12;
+
+    @FXML
+    private ImageView E11;
+
+    @FXML
+    private ImageView E13;
+
+    @FXML
+    private ImageView E14;
+
+    @FXML
+    private ImageView E15;
+
+    @FXML
+    private ImageView E16;
+
+    @FXML
+    private ImageView E17;
+
+    @FXML
+    private ImageView E18;
+
+    @FXML
+    private ImageView E21;
+
+    @FXML
+    private ImageView E22;
+
+    @FXML
+    private ImageView E24;
+
+    @FXML
+    private ImageView E25;
+
+    @FXML
+    private ImageView E28;
+
+    @FXML
+    private ImageView E23;
+
+    @FXML
+    private ImageView E26;
+
+    @FXML
+    private ImageView E27;
+
+    @FXML
+    private ImageView E31;
+
+    @FXML
+    private ImageView E32;
+
+    @FXML
+    private ImageView E39;
+
+    @FXML
+    private ImageView E34;
+
+    @FXML
+    private ImageView E33;
+
+    @FXML
+    private ImageView E36;
+
+    @FXML
+    private ImageView E37;
+
+    @FXML
+    private ImageView E38;
+
+    @FXML
+    private ImageView E35;
+
+    @FXML
+    void B11(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B11.setImage(img);
+
+    }
+
+    @FXML
+    void B12(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B12.setImage(img);
+
+    }
+
+    @FXML
+    void B13(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B13.setImage(img);
+
+    }
+
+    @FXML
+    void B14(MouseEvent event) {
+
+        Image img=new Image("sample/final_chair.png");
+        B14.setImage(img);
+    }
+
+    @FXML
+    void B15(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void B16(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B16.setImage(img);
+
+    }
+
+    @FXML
+    void B17(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+
+    }
+
+    @FXML
+    void B18(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void B21(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B21.setImage(img);
+
+    }
+
+    @FXML
+    void B22(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void B23(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void B24(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B24.setImage(img);
+
+    }
+
+    @FXML
+    void B25(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B25.setImage(img);
+
+    }
+
+    @FXML
+    void B26(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B26.setImage(img);
+
+    }
+
+    @FXML
+    void B27(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B27.setImage(img);
+
+    }
+
+    @FXML
+    void B28(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        B28.setImage(img);
+
+    }
+
+    @FXML
+    void E11(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E11.setImage(img);
+
+    }
+
+    @FXML
+    void E12(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E12.setImage(img);
+
+    }
+
+    @FXML
+    void E13(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E13.setImage(img);
+
+    }
+
+    @FXML
+    void E14(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E14.setImage(img);
+
+    }
+
+    @FXML
+    void E15(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E15.setImage(img);
+
+    }
+
+    @FXML
+    void E16(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E16.setImage(img);
+
+    }
+
+    @FXML
+    void E17(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E18(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E21(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E22(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E23(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E23.setImage(img);
+
+    }
+
+    @FXML
+    void E24(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E25(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E26(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E26.setImage(img);
+
+    }
+
+    @FXML
+    void E27(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E27.setImage(img);
+
+    }
+
+    @FXML
+    void E28(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E31(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E32(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E33(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E33.setImage(img);
+
+    }
+
+    @FXML
+    void E34(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void E35(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E35.setImage(img);
+
+    }
+
+    @FXML
+    void E36(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E36.setImage(img);
+
+    }
+
+    @FXML
+    void E37(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E37.setImage(img);
+
+    }
+
+    @FXML
+    void E38(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        E38.setImage(img);
+
+    }
+
+    @FXML
+    void E39(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
     void F11(MouseEvent event) {
-        Image img=new Image("sample/icons8-theatre-seat-80_black.png");
+        Image img=new Image("sample/final_chair.png");
         F11.setImage(img);
 
     }
 
     @FXML
     void F12(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F12.setImage(img);
 
     }
 
     @FXML
     void F13(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F13.setImage(img);
 
     }
 
     @FXML
     void F14(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F14.setImage(img);
 
     }
 
+    @FXML
+    void F15(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
+    void F16(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F16.setImage(img);
+
+    }
+
+    @FXML
+    void F17(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F17.setImage(img);
+
+    }
+
+    @FXML
+    void F18(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
 
     @FXML
     void F21(MouseEvent event) {
@@ -60,12 +591,57 @@ public class seatViewController {
     }
 
     @FXML
+    void F22(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
+
+    }
+
+    @FXML
     void F23(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F23.setImage(img);
 
     }
 
     @FXML
     void F24(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F24.setImage(img);
+
+    }
+
+    @FXML
+    void F25(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F25.setImage(img);
+
+    }
+
+    @FXML
+    void F26(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F26.setImage(img);
+
+    }
+
+    @FXML
+    void F27(MouseEvent event) {
+        Image img=new Image("sample/final_chair.png");
+        F27.setImage(img);
+
+    }
+
+    @FXML
+    void F28(MouseEvent event) {
+        Toolkit.getDefaultToolkit().beep();
+        Alert Logout=new Alert(Alert.AlertType.WARNING);
+        Logout.setContentText("Seat Not Available");
+        Logout.setHeaderText("Seat Info");
+        Logout.show();
 
     }
 
